ba01279e6a8245656c5ca0a02cff1effc78e14c5
benwhomsley <whomsleydesign@live.co.uk>
2025-09-28 16:06:52 +0100

    Update moody moose media url

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
-import { Project,Experience } from "./types";
+import { Project, Experience } from "./types";
 
 export const projects: Project[] = [
   {
@@ -32,7 +32,7 @@ export const projects: Project[] = [
   {
     label: "Drone photography",
     title: "A media companies brochure site",
-    url: "https://effortless-maamoul-af63e9.netlify.app/",
+    url: "https://moodymoosemedia.com/",
     image: "/images/projects/drone.png",
     icon: "/images/projects/icons/drone.png",
   },
@@ -43,14 +43,15 @@ export const projects: Project[] = [
     image: "/images/projects/beep.png",
     icon: "/images/projects/icons/beep.ico",
   },
-]
+];
 
 export const experience: Experience[] = [
   {
     time: "2022 - present",
     title: "Student Beans / Pion",
     url: "https://studentbeans.com",
-    description: "Currently part of the team that builds and maintains the web platform for the #1 student loyalty network. As well as working on other internal and external projects, my role also extends to mentoring other engineers and helping them grow in their careers.",
+    description:
+      "Currently part of the team that builds and maintains the web platform for the #1 student loyalty network. As well as working on other internal and external projects, my role also extends to mentoring other engineers and helping them grow in their careers.",
     roles: [
       { name: "Senior JavaScript Engineer", duration: "~ 7 months" },
       { name: "JavaScript Engineer", duration: "2 years 5 months" },
@@ -62,7 +63,8 @@ export const experience: Experience[] = [
     time: "2017 - 2022",
     title: "Electroneum",
     url: "https://electroneum.com",
-    description: "Worked as part of a startup team building a new digital ecosystem and launching one of the most successful crypto projects of it's time. Leading the UX unit, we brought a revolutionary new digital eco system to market giving unbanked people in emerging economies access to a digital payment system.",
+    description:
+      "Worked as part of a startup team building a new digital ecosystem and launching one of the most successful crypto projects of it's time. Leading the UX unit, we brought a revolutionary new digital eco system to market giving unbanked people in emerging economies access to a digital payment system.",
     roles: [{ name: "UX Developer", duration: "~ 5 years" }],
     coreTech: ["Angular", "Ionic", "SASS", "HTML", "Jest", "Git"],
     relatedTech: ["AWS", "Docker", "Node"],
@@ -70,7 +72,8 @@ export const experience: Experience[] = [
   {
     time: "2012 - 2017",
     title: "Creative agencies",
-    description: "During this time I worked across three separate creative agencies working on a range of different products and mediums, ranging from print collateral for real estate marketing, to performance management apps for professional sports organisations (Team GB Taekwondo, Saracens rugby club, ECB Cricket and many more), to custom social media management dashboards.",
+    description:
+      "During this time I worked across three separate creative agencies working on a range of different products and mediums, ranging from print collateral for real estate marketing, to performance management apps for professional sports organisations (Team GB Taekwondo, Saracens rugby club, ECB Cricket and many more), to custom social media management dashboards.",
     roles: [
       { name: "UI/UX Designer", duration: "~ 2 years" },
       { name: "Web & Application Designer", duration: "~ 1 year" },
@@ -79,4 +82,4 @@ export const experience: Experience[] = [
     coreTech: ["Adobe Suite", "Wordpress", "CSS", "HTML"],
     relatedTech: ["PHP"],
   },
-]
+];
